74713b4ef6ebea89a650f8d1caab3615f2aaab1e
Luca Carvalho <lucarlcarvalho@outlook.com>
2021-09-13 20:28:59 -0300

    Copyright notices added

--- components/TopNavbar.jsx
+++ components/TopNavbar.jsx
@@ -1,3 +1,8 @@
+//          Copyright Luca R. L. de Carvalho 2021.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE or copy at
+//          https://www.boost.org/LICENSE_1_0.txt)
+
 import React from "react";
 import { StyleSheet, Text } from "react-native";
 import SimpleButton from "./SimpleButton";
